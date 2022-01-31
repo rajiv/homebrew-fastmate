66ca208f56c8dc3bc2fd3383b5370e696c6d27b3
Rajiv Aaron Manglani <rajiv@alum.mit.edu>
2022-01-30 23:03:16 -0500

    Fastmate 1.8.1.
    * Fastmate now requires MacOS 10.15 Catalina or higher.

--- Casks/fastmate.rb
+++ Casks/fastmate.rb
@@ -1,9 +1,15 @@
 cask "fastmate" do
-  version "1.8.0"
-  sha256 "b77a55c111cb42e3ca195638317a86061b1163e51138cc0cbddcee44c6f9e41e"
+  if MacOS.version <= :mojave
+    version "1.8.0"
+    sha256 "b77a55c111cb42e3ca195638317a86061b1163e51138cc0cbddcee44c6f9e41e"
+    url "https://github.com/joelekstrom/fastmate/releases/download/v#{version}/Fastmate-#{version}.zip"
+  else
+    version "1.8.1"
+    sha256 "4508e65eb6a85fbc6ff315c8d074bae5ead48a6b204220d940be6a2d49ab065b"
+    url "https://github.com/joelekstrom/fastmate/releases/download/v#{version}/Fastmate-#{version}.zip"
+    appcast "https://github.com/joelekstrom/fastmate/releases.atom"
+  end
 
-  url "https://github.com/joelekstrom/fastmate/releases/download/v#{version}/Fastmate-#{version}.zip"
-  appcast "https://github.com/joelekstrom/fastmate/releases.atom"
   name "Fastmate"
   desc "Native wrapper for Fastmail"
   homepage "https://github.com/joelekstrom/fastmate"
