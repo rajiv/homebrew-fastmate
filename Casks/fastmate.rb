8644ba2b22f4a1b6f263f516325431d47ab446ef
Rajiv Aaron Manglani <rajiv@alum.mit.edu>
2022-02-11 10:20:50 -0500

    Fastmate 1.8.2.
    * Update signature for notarized version of Fastmate 1.8.0.

--- Casks/fastmate.rb
+++ Casks/fastmate.rb
@@ -1,11 +1,11 @@
 cask "fastmate" do
   if MacOS.version <= :mojave
     version "1.8.0"
-    sha256 "b77a55c111cb42e3ca195638317a86061b1163e51138cc0cbddcee44c6f9e41e"
+    sha256 "54e46f5c192c941a0c4f27364def9f1eee36850047c331a66377e3d151efeffe"
     url "https://github.com/joelekstrom/fastmate/releases/download/v#{version}/Fastmate-#{version}.zip"
   else
-    version "1.8.1"
-    sha256 "4508e65eb6a85fbc6ff315c8d074bae5ead48a6b204220d940be6a2d49ab065b"
+    version "1.8.2"
+    sha256 "6eb95d02f54bdfc08b462eae4f5c3bab76e5fa3c530f8b771d2dba6c424a1692"
     url "https://github.com/joelekstrom/fastmate/releases/download/v#{version}/Fastmate-#{version}.zip"
     appcast "https://github.com/joelekstrom/fastmate/releases.atom"
   end
