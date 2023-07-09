4456f4ad6180dd88d9eb53ccc634d18adada1b48
Rajiv Aaron Manglani <rajiv@alum.mit.edu>
2023-07-09 11:11:43 -0400

    Cleanup brew style.

--- Casks/fastmate.rb
+++ Casks/fastmate.rb
@@ -1,11 +1,18 @@
 cask "fastmate" do
-  if MacOS.version <= :mojave
+  on_mojave :or_older do
     version "1.8.0"
     sha256 "54e46f5c192c941a0c4f27364def9f1eee36850047c331a66377e3d151efeffe"
+
     url "https://github.com/joelekstrom/fastmate/releases/download/v#{version}/Fastmate-#{version}.zip"
-  else
+
+    livecheck do
+      skip "Legacy version"
+    end
+  end
+  on_catalina :or_newer do
     version "1.9.1"
     sha256 "8e8555fbe75232e9c182f4a11cc1c5447bd75f28ed24d407d3d6cb4d5e8f8538"
+
     url "https://github.com/joelekstrom/fastmate/releases/download/v#{version}/Fastmate-#{version}.zip"
 
     livecheck do
