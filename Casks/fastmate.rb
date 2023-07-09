ad9215b83250d9c1dbc773ff583ffb204671cbf7
Rajiv Aaron Manglani <rajiv@alum.mit.edu>
2023-07-09 10:30:35 -0400

    Replace deprecated `appcast` with `livecheck`.
    Fixes #1.

--- Casks/fastmate.rb
+++ Casks/fastmate.rb
@@ -7,7 +7,10 @@ cask "fastmate" do
     version "1.9.1"
     sha256 "8e8555fbe75232e9c182f4a11cc1c5447bd75f28ed24d407d3d6cb4d5e8f8538"
     url "https://github.com/joelekstrom/fastmate/releases/download/v#{version}/Fastmate-#{version}.zip"
-    appcast "https://github.com/joelekstrom/fastmate/releases.atom"
+
+    livecheck do
+      url :url
+    end
   end
 
   name "Fastmate"
